1df8658359b3f6b2c063472f55b0c213415e0662
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2023-08-16 18:04:51 +0200

    Création des routes + mise en place header et footer

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Logements from './pages/Logements'
+import About from './pages/About'
+import Error from './pages/Error'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer'
+import './styles/Global.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={< Home />} />
+        <Route path="/logements" element={< Logements />} />
+        <Route path="/about" element={< About /> } />
+        <Route path="*" element={< Error />} />
+      </Routes>
+      <Footer />
+    </Router>
   </React.StrictMode>
 );
